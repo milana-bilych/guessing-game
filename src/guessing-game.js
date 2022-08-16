02297e7d00cecbcf874f174191648b7e36192d33
milana-bilych <akimmilana@gmail.com>
2022-08-16 17:41:18 +0300

    feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.total = Math.ceil((this.max + this.min) / 2);
+        return this.total
     }
 
     lower() {
-
+        this.max = this.total
     }
 
     greater() {
-
+        this.min = this.total
     }
 }
 
